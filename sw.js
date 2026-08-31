62e71f81393fd60746fa9c703b4af7ea7574bd71
YUKI-NOTE\yukii <ykishikura@gmail.com>
2026-09-01 03:32:40 +0900

    deploy01

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'sht-calendar-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './SHTCalendar.html',
+  './icon.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
